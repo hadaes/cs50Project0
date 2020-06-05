7b4c225f3e65670f27415593000399d75c43c06f
Jack Scherlag <jackscherlag@Jacks-MacBook-Pro.local>
2020-06-05 18:21:48 -0500

    Added class variable discovered to allow for more efficient search. Added a new function discovered_tuple.

--- degrees/util.py
+++ degrees/util.py
@@ -6,14 +6,25 @@ class Node():
 
 
 class StackFrontier():
+
+    discovered = []
+
     def __init__(self):
         self.frontier = []
 
     def add(self, node):
         self.frontier.append(node)
+        x = (node.action, node.state)
+        self.discovered.append(x)
 
     def contains_state(self, state):
         return any(node.state == state for node in self.frontier)
+
+    def discovered_tuple(self, x):
+        if x in self.discovered:
+            return 1
+        else:
+            return 0
 
     def empty(self):
         return len(self.frontier) == 0
